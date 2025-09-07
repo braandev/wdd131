a99da42a56406fd0216de65fe9fb440177b272f4
braandev <brandondev.contacto@gmail.com>
2025-09-06 23:45:46 -0300

    First commit

---
+++ scripts/getdates.js
@@ -0,0 +1,3 @@
+// Populate current year and last modified
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+document.getElementById("lastModified").textContent = `Last Modification: ${document.lastModified}`;
