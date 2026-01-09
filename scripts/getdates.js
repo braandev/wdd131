89eb58e9c822ed1812ed804127cf33bdb3d8f735
braandev <brandondev.contacto@gmail.com>
2026-01-08 22:02:54 -0300

    first commit

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+const currentYear = new Date().getFullYear();
+
+document.getElementById("currentYear").innerHTML = `© ${currentYear} 🧑‍💻 Brandon Cazorla 🧑‍💻 Argentina`;
+
+document.getElementById("lastModified").innerHTML = `Last Modified: ${document.lastModified}`;
