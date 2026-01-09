848a23532277be23e5da48deecdcd06cb284c474
braandev <brandondev.contacto@gmail.com>
2026-01-09 01:10:52 -0300

    changing some things

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,5 +1,6 @@
-const currentYear = new Date().getFullYear();
-
-document.getElementById("currentYear").innerHTML = `© ${currentYear} 🧑‍💻 Brandon Cazorla 🧑‍💻 Argentina`;
+const year = new Date().getFullYear();
+document.getElementById("currentyear").innerHTML = `© ${year} 🧑‍💻 Brandon Cazorla 🧑‍💻 Argentina`;
 
 document.getElementById("lastModified").innerHTML = `Last Modified: ${document.lastModified}`;
+
+document.getElementById("course-title").textContent
